c7c126da5c064106d8a01943cec8b8dd135e23b5
Alina Sansevich <alinasansevich@gmail.com>
2021-08-19 15:10:43 -0500

    Added entries views, templates, urls.

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -7,10 +7,19 @@ Created on Thu Aug 19 13:36:10 2021
 """
 
 from django import forms
-from .models import Topic
+from .models import Entry, Topic
+
 
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
         labels = {'text': ''}
+
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': 'blablabla'}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
