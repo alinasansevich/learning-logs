d7a7707a69540c2a1210a4c58c5306fa839b5348
Alina Sansevich <alinasansevich@gmail.com>
2021-09-02 11:53:46 -0500

    Added access restrictions.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
  
 from .models import Entry, Topic
 from .forms import EntryForm, TopicForm
@@ -11,9 +12,10 @@ def index(request):
     return render(request, 'learning_logs/index.html') # render takes the request as first argument, and a template it uses to build the page.
 
 
+@login_required
 def topics(request):
     """Show all topics."""
-    topics = Topic.objects.order_by('date_added') # &*&*&
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added') # &*&*&
     context = {'topics': topics} # this dict contains the data needed to build the webpage with the template
     return render(request, 'learning_logs/topics.html', context)
 # &*&*&
@@ -21,14 +23,20 @@ def topics(request):
 # sorted by the date_added attribute. We store the resulting queryset in topics.
 
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id) # this is  a query
+    # Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
+        
     entries = topic.entry_set.order_by('-date_added') # '-date_added' the '-' in front of 'date_added' sorts the results in reverse order --> it will display the most recent entries first
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     """Add a new topic."""
     if request.method != 'POST':
@@ -38,13 +46,16 @@ def new_topic(request):
         # POST data submitted; process data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
     
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
@@ -65,10 +76,13 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
     
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
